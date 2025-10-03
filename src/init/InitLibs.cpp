fd6c2ac1e9e176e9607233bfe7a0d9ea1b251382
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-10-03 17:19:18 +0100

    First commit

--- src/init/InitLibs.cpp
+++ src/init/InitLibs.cpp
@@ -11,7 +11,6 @@ void InitLibs::initAll(int _argc, char* _argv[]) {
         curl_global_init(CURL_GLOBAL_DEFAULT);
         FLAGS_logtostderr = 1;
         FLAGS_minloglevel = google::GLOG_INFO;
-        google::InitGoogleLogging(_argv[0]);
         static folly::Init init(&_argc, &_argv);  // Static to preserve lifetime, pass by pointer
     }
 }
