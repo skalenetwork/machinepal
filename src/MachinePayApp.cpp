6982a15970e4a960556f6a94ea53a1f379a7849f
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-10-13 12:31:10 +0100

    First commit

--- src/MachinePayApp.cpp
+++ src/MachinePayApp.cpp
@@ -23,12 +23,32 @@ uint32_t MachinePayApp::runUntilExit()
         std::make_shared<folly::NamedThreadFactory>("x402Processor"));
 
         spdlog::info("Starting server");
-        proxygenServer_->start(nullptr, nullptr, nullptr,  ioExecutor);
-        spdlog::info("Server exited normally");
+
+        auto onSuccess = []() {
+            spdlog::info("Machine pay server started successfully.");
+        };
+        auto onError = [](std::exception_ptr eptr) {
+            try {
+                if (eptr) std::rethrow_exception(eptr);
+            } catch (const std::exception& ex) {
+                spdlog::error("Proxygen server failed to start: {}", ex.what());
+            } catch (...) {
+                spdlog::error("Proxygen server failed to start: unknown error");
+            }
+        };
+        std::thread serverThread([this, ioExecutor, onSuccess, onError]() {
+            try {
+                proxygenServer_->start(onSuccess, onError, nullptr, ioExecutor);
+            } catch (...) {
+                onError(std::current_exception());
+            }
+        });
+        serverThread.join();
+        spdlog::info("Machinepay server exited normally");
         return 0;
     } catch (const std::exception& ex)
     {
-        spdlog::critical("Fatal error running server: {}. Server exited", ex.what());
+        spdlog::critical("Fatal error running machinepay server: {}. Server exited", ex.what());
         printNestedException(ex);
         return 1;
     }
