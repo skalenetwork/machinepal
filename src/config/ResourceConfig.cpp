9c3de3a28eda2c8c5c14f6873459c55ee7127464
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-10-17 19:04:54 +0100

    First commit

--- src/config/ResourceConfig.cpp
+++ src/config/ResourceConfig.cpp
@@ -2,25 +2,34 @@
 
 class FileManager;
 
-ptr<ResourceConfig> ResourceConfig::createFromJson(const nlohmann::json& j, ptr<FileManager> fileManager) {
-    CHECK_STATE(fileManager);
-    std::string name = j.value("name", "");
-    std::string location = j.value("location", "");
-    ResourceType type = resourceTypeFromString(j.value("type", ""));
-    return ptr<ResourceConfig>(new ResourceConfig(name, location, type));
+ptr<ResourceConfig> ResourceConfig::createFromJson(const nlohmann::json &j, ptr<FileManager> fileManager) {
+    try {
+        CHECK_STATE(fileManager);
+        std::string name = j.value("name", "");
+        std::string location = j.value("location", "");
+        ResourceType type = resourceTypeFromString(j.value("type", ""));
+        return ptr<ResourceConfig>(new ResourceConfig(name, location, type));
+    } catch (const std::exception &ex) {
+        RETHROW_NESTED;
+    }
 }
 
-ptr<vector<ptr<ResourceConfig>>> ResourceConfig::createVectorFromJsonArray(const nlohmann::json& j, ptr<FileManager> fileManager) {
-    CHECK_STATE(fileManager);
-    auto result = std::make_shared<std::vector<ptr<ResourceConfig>>>();
-    if (!j.contains("resources"))
+ptr<vector<ptr<ResourceConfig> > > ResourceConfig::createVectorFromJsonArray(
+    const nlohmann::json &j, ptr<FileManager> fileManager) {
+    try {
+        CHECK_STATE(fileManager);
+        auto result = std::make_shared<std::vector<ptr<ResourceConfig> > >();
+        if (!j.contains("resources"))
+            return result;
+        auto resources = j.at("resources");
+        CHECK_STATE(resources.is_array());
+        for (const auto &item: resources) {
+            auto res = createFromJson(item, fileManager);
+            CHECK_STATE(res);
+            result->push_back(res);
+        }
         return result;
-    auto resources = j.at("resources");
-    CHECK_STATE(resources.is_array());
-    for (const auto& item : resources) {
-        auto res = createFromJson(item, fileManager);
-        CHECK_STATE(res);
-        result->push_back(res);
+    } catch (const std::exception &ex) {
+        RETHROW_NESTED;
     }
-    return result;
 }
