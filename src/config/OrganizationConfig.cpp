9afe8c37eae0734905e85b4dcc56db3bd064943d
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-10-17 19:07:10 +0100

    First commit

--- src/config/OrganizationConfig.cpp
+++ src/config/OrganizationConfig.cpp
@@ -1,14 +1,19 @@
 #include "OrganizationConfig.h"
 
 ptr<OrganizationConfig> OrganizationConfig::createFromJson(const nlohmann::json& j, ptr<FileManager> fileManager) {
-    CHECK_STATE(fileManager);
-    std::string organizationName = j.value("name", "");
-    auto resources = ResourceConfig::createVectorFromJsonArray(j, fileManager);
-    return ptr<OrganizationConfig>(new OrganizationConfig(resources, organizationName));
+    try {
+        CHECK_STATE(fileManager);
+        std::string organizationName = j.value("name", "");
+        auto resources = ResourceConfig::createVectorFromJsonArray(j, fileManager);
+        return ptr<OrganizationConfig>(new OrganizationConfig(resources, organizationName));
+    } catch (const std::exception& ex) {
+        RETHROW_NESTED;
+    }
 }
 
 std::shared_ptr<std::vector<ptr<OrganizationConfig>>> OrganizationConfig::createVectorFromJsonArray(const nlohmann::json& j, ptr<FileManager> fileManager) {
     try {
+        CHECK_STATE(fileManager);
         auto result = std::make_shared<std::vector<ptr<OrganizationConfig>>>();
         if (!j.contains("organizations"))
             return result;
@@ -25,5 +30,9 @@ std::shared_ptr<std::vector<ptr<OrganizationConfig>>> OrganizationConfig::create
 }
 
 ptr<OrganizationConfig> OrganizationConfig::createDefaultFromResources(ptr<vector<ptr<ResourceConfig>>> resources) {
-    return ptr<OrganizationConfig>(new OrganizationConfig(resources, ""));
+    try {
+        return ptr<OrganizationConfig>(new OrganizationConfig(resources, ""));
+    } catch (const std::exception& ex) {
+        RETHROW_NESTED;
+    }
 }
