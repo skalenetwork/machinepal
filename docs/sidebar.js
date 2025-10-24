6356b85d8d1759658f89703a7b19b76038e62553
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-10-24 15:32:29 +0100

    First commit

---
+++ docs/sidebar.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tutorialSidebar: [
+        {
+            type: 'doc',
+            id: 'intro', // corresponds to docs/docs/intro.md
+            label: 'Introduction',
+        },
+        // ... other sidebar items
+    ],
+};
