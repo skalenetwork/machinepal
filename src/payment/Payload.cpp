6567c652d3b375b83841da37d9d9578228e9bf94
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-10-24 16:46:48 +0100

    Enhance Payload::fromJson method with improved error handling and validation

--- src/payment/Payload.cpp
+++ src/payment/Payload.cpp
@@ -1,6 +1,8 @@
 #include "Payload.h"
 #include <stdexcept>
 
+#include "config/JsonUtils.h"
+
 // Payload implementations
 Payload::Payload() = default;
 
@@ -21,13 +23,22 @@ bool Payload::operator==(const Payload& other) const {
 }
 
 std::shared_ptr<Payload> Payload::fromJson(const json& j) {
-    if (!j.contains("signature") || !j.contains("authorization")) {
-        throw std::invalid_argument("Missing required field in Payload JSON");
+    try {
+        CHECK_STATE_JSON(j.contains("signature"), "Missing required field 'signature' in Payload JSON", j);
+        CHECK_STATE_JSON(j.contains("authorization"), "Missing required field 'authorization' in Payload JSON", j);
+
+        CHECK_STATE_JSON(j.at("signature").is_string(), "'signature' must be a string in Payload JSON", j);
+        CHECK_STATE_JSON(j.at("authorization").is_object(), "'authorization' must be an object in Payload JSON", j);
+
+
+
+        return std::make_shared<Payload>(
+            j.at("signature").get<std::string>(),
+            *Authorization::fromJson(j.at("authorization"))
+        );
+    } catch (std::exception& e) {
+        RETHROW_NESTED;
     }
-    return std::make_shared<Payload>(
-        j.at("signature").get<std::string>(),
-        *Authorization::fromJson(j.at("authorization"))
-    );
 }
 
 json Payload::toJson() const {
