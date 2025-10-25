038bb991ed5381dac2eaa3f75b24e04e90258f9a
kladkogex <13399135+kladkogex@users.noreply.github.com>
2025-10-25 19:41:40 +0100

    Add EthPublicKey class and update CryptoManager to derive public keys

--- src/crypto/EthPublicKey.cpp
+++ src/crypto/EthPublicKey.cpp
@@ -65,9 +65,14 @@ std::string EthPublicKey::toHex() const {
     CHECK_STATE2(hexString.size() == 132, "Invalid hex string size"); // 2 for '0x' + 2 for prefix + 128 for 64 bytes
     return hexString;
 }
+
 EthAddress EthPublicKey::getAddress() const {
-    auto hash = keccak::keccak256(std::span<const uint8_t>(bytes_.data(), 64));
+    std::array<uint8_t, 65> uncompressed{};
+    uncompressed[0] = 0x04;
+    std::copy(bytes_.begin(), bytes_.end(), uncompressed.begin() + 1);
+    auto hash = keccak::keccak256(std::span<const uint8_t>(uncompressed.data(), 65));
     return EthAddress(hash.data() + 12, 20);
 }
+
 bool operator==(const EthPublicKey& a, const EthPublicKey& b) { return a.bytes_ == b.bytes_; }
 bool operator!=(const EthPublicKey& a, const EthPublicKey& b) { return !(a == b); }
