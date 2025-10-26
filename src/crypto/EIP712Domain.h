ffc79c54f877ab0a03202912cc68ce2c53cbb106
kladkogex <13399135+kladkogex@users.noreply.github.com>
2025-10-26 22:48:20 +0000

    Refactor EIP-712 domain class to streamline constructor and add static type hashes

--- src/crypto/EIP712Domain.h
+++ src/crypto/EIP712Domain.h
@@ -10,8 +10,66 @@
 using u256 = boost::multiprecision::uint256_t;
 
 class EIP712Domain {
-
     std::string name_;
+    std::string version_;
+    u256 chainId_;
+    EthAddress verifyingContract_;
+    std::string domainSeparator_;
+
+
+public:
+
+    static constexpr const char *CANCEL_AUTHORIZATION_TYPE_HASH =
+        "0x158b0a9edf7a828aad02f63cd515c68ef2f50ba807396f6d12842833a1597429";
+    static constexpr const char *PERMIT_TYPE_HASH =
+        "0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9";
+    static constexpr const char *RECEIVE_WITH_AUTHORIZATION_TYPE_HASH =
+        "0xd099cc98ef71107a616c4f0f941f04c322d8e254fe26b3c6668db87aae413de8";
+    static constexpr const char *TRANSFER_WITH_AUTHORIZATION_TYPE_HASH =
+        "0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267";
+
+
+
+    // Default constructor
+    EIP712Domain() = default;
+
+    EIP712Domain(const std::string &name, const std::string &version, const u256 &chainId,
+                 const EthAddress &verifyingContract, const std::string &domainSeparator)
+        : name_(name),
+          version_(version),
+          chainId_(chainId),
+          verifyingContract_(verifyingContract),
+          domainSeparator_(domainSeparator){
+    }
+
+
+    // Computes the EIP-712 domain separator hash
+    std::array<uint8_t, 32> hashDomain() const;
+
+    static std::array<uint8_t, 32> getDomainTypeHash();
+
+
+    static EIP712Domain machinePayEasyTestNet() {
+        return EIP712Domain{"USDC", "2", 84542,
+                            EthAddress("0x036CbD53842c5426634e7929541eC2318f3dCF7e"),
+                            "0x71f17a3b2ff373b803d70a5a07c046c1a2bc8e89c09ef722fcb047abe94c9818",
+
+        };
+    }
+
+    static EIP712Domain baseSepolia() {
+        return EIP712Domain{"USDC", "2", 84532,
+                            EthAddress("0x036CbD53842c5426634e7929541eC2318f3dCF7e"),
+                            "0x71f17a3b2ff373b803d70a5a07c046c1a2bc8e89c09ef722fcb047abe94c9818"
+        };
+    }
+
+
+    static EIP712Domain baseMainnet() {
+        return {"USD Coin", "2", 8453,
+                EthAddress("0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"),
+                "0x02fa7265e7c5d81118673727957699e4d68f74cd74b7db77da710fe8a2c7834f"};
+    }
 
 public:
     [[nodiscard]] std::string name() const {
@@ -34,94 +92,5 @@ public:
         return domainSeparator_;
     }
 
-    [[nodiscard]] std::string cancelAuthorizationTypeHash() const {
-        return cancelAuthorizationTypeHash_;
-    }
-
-    [[nodiscard]] std::string permitTypeHash() const {
-        return permitTypeHash_;
-    }
-
-    [[nodiscard]] std::string receiveWithAuthorizationTypeHash() const {
-        return receiveWithAuthorizationTypeHash_;
-    }
-
-    [[nodiscard]] std::string transferWithAuthorizationTypeHash() const {
-        return transferWithAuthorizationTypeHash_;
-    }
-
-private:
-    std::string version_;
-    u256 chainId_;
-    EthAddress verifyingContract_;
-    std::string domainSeparator_;
-    std::string cancelAuthorizationTypeHash_;
-    std::string permitTypeHash_;
-    std::string receiveWithAuthorizationTypeHash_;
-    std::string transferWithAuthorizationTypeHash_;
-
-public:
-
-
-
-    // Default constructor
-    EIP712Domain() = default;
-
-    EIP712Domain(const std::string& name, const std::string& version, const u256& chainId,
-        const EthAddress& verifyingContract, const std::string& domainSeparator,
-        const std::string& cancelAuthorizationTypeHash, const std::string& permitTypeHash,
-        const std::string& receiveWithAuthorizationTypeHash, const std::string& transferWithAuthorizationTypeHash)
-        : name_(name),
-          version_(version),
-          chainId_(chainId),
-          verifyingContract_(verifyingContract),
-          domainSeparator_(domainSeparator),
-          cancelAuthorizationTypeHash_(cancelAuthorizationTypeHash),
-          permitTypeHash_(permitTypeHash),
-          receiveWithAuthorizationTypeHash_(receiveWithAuthorizationTypeHash),
-          transferWithAuthorizationTypeHash_(transferWithAuthorizationTypeHash) {
-    }
-
-
-    // Computes the EIP-712 domain separator hash
-    std::array<uint8_t, 32> hashDomain() const;
-
-    static std::array<uint8_t, 32> getDomainTypeHash();
-
-
-    static const EIP712Domain machinePayEasyTestNet() {
-        return  EIP712Domain{"USDC", "2", 84542,
-                               EthAddress("0x036CbD53842c5426634e7929541eC2318f3dCF7e"),
-            "0x71f17a3b2ff373b803d70a5a07c046c1a2bc8e89c09ef722fcb047abe94c9818",
-            "0x158b0a9edf7a828aad02f63cd515c68ef2f50ba807396f6d12842833a1597429",
-            "0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9",
-            "0xd099cc98ef71107a616c4f0f941f04c322d8e254fe26b3c6668db87aae413de8",
-            "0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267",
-
-        };
-    }
-
-    static const EIP712Domain baseSepolia() {
-        return  EIP712Domain{"USDC", "2", 84532,
-                               EthAddress("0x036CbD53842c5426634e7929541eC2318f3dCF7e"),
-            "0x71f17a3b2ff373b803d70a5a07c046c1a2bc8e89c09ef722fcb047abe94c9818",
-            "0x158b0a9edf7a828aad02f63cd515c68ef2f50ba807396f6d12842833a1597429",
-            "0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9",
-            "0xd099cc98ef71107a616c4f0f941f04c322d8e254fe26b3c6668db87aae413de8",
-            "0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267",
-        };
-    }
-
-
-    static const EIP712Domain baseMainnet() {
-        return  {"USD Coin", "2", 8453,
-                               EthAddress("0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"),
-        "0x02fa7265e7c5d81118673727957699e4d68f74cd74b7db77da710fe8a2c7834f",
-        "0x158b0a9edf7a828aad02f63cd515c68ef2f50ba807396f6d12842833a1597429",
-            "0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9",
-            "0xd099cc98ef71107a616c4f0f941f04c322d8e254fe26b3c6668db87aae413de8",
-            "0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267"
-        };
-    }
 
 };
