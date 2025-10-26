ccfc6fdc59f4327456e126a1f0597ef4a35bb6fb
kladkogex <13399135+kladkogex@users.noreply.github.com>
2025-10-26 22:35:19 +0000

    Refactor EIP-712 domain hashing and add hex conversion utility

--- src/crypto/EIP712Domain.h
+++ src/crypto/EIP712Domain.h
@@ -90,19 +90,20 @@ public:
 
 
     static const EIP712Domain machinePayEasyTestNet() {
-        return  EIP712Domain{"USD", "2", 84542,
-                               EthAddress("0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"),
+        return  EIP712Domain{"USDC", "2", 84542,
+                               EthAddress("0x036CbD53842c5426634e7929541eC2318f3dCF7e"),
             "0x71f17a3b2ff373b803d70a5a07c046c1a2bc8e89c09ef722fcb047abe94c9818",
             "0x158b0a9edf7a828aad02f63cd515c68ef2f50ba807396f6d12842833a1597429",
             "0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9",
             "0xd099cc98ef71107a616c4f0f941f04c322d8e254fe26b3c6668db87aae413de8",
             "0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267",
+
         };
     }
 
     static const EIP712Domain baseSepolia() {
-        return  EIP712Domain{"USD", "2", 84532,
-                               EthAddress("0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"),
+        return  EIP712Domain{"USDC", "2", 84532,
+                               EthAddress("0x036CbD53842c5426634e7929541eC2318f3dCF7e"),
             "0x71f17a3b2ff373b803d70a5a07c046c1a2bc8e89c09ef722fcb047abe94c9818",
             "0x158b0a9edf7a828aad02f63cd515c68ef2f50ba807396f6d12842833a1597429",
             "0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9",
@@ -110,10 +111,6 @@ public:
             "0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267",
         };
     }
-
-
-
-
 
 
     static const EIP712Domain baseMainnet() {
