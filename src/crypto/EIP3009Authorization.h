c466673f058b0f3612272e893ccd7dc1c6e5d4da
kladkogex <13399135+kladkogex@users.noreply.github.com>
2025-10-26 23:03:01 +0000

    Refactor EIP-712 domain hashing and add hex conversion utility

--- src/crypto/EIP3009Authorization.h
+++ src/crypto/EIP3009Authorization.h
@@ -31,4 +31,13 @@ public:
                                     const std::string& nonce,
                                     const EIP712Signature& signature,
                                     const EthPublicKey& publicKey);
+
+    static constexpr const char *PERMIT_TYPE_HASH =
+    "0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9";
+    static constexpr const char *RECEIVE_WITH_AUTHORIZATION_TYPE_HASH =
+        "0xd099cc98ef71107a616c4f0f941f04c322d8e254fe26b3c6668db87aae413de8";
+    static constexpr const char *TRANSFER_WITH_AUTHORIZATION_TYPE_HASH =
+        "0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267";
+    static constexpr const char *CANCEL_AUTHORIZATION_TYPE_HASH =
+        "0x158b0a9edf7a828aad02f63cd515c68ef2f50ba807396f6d12842833a1597429";
 };
