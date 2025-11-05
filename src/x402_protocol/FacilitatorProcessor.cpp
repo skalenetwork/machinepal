736b6981128da76a101b86070760b8331f668f72
kladkogex <13399135+kladkogex@users.noreply.github.com>
2025-11-05 23:36:15 +0000

    Add FacilitatorProcessor class for handling payment requests and responses

---
+++ src/x402_protocol/FacilitatorProcessor.cpp
@@ -0,0 +1,170 @@
+
+#include "FacilitatorProcessor.h"
+#include "BackendConnection.h"
+#include "IResponseSender.h"
+#include "MachinePayApp.h"
+#include "MachinePayCommon.h"
+#include "config/subconfigs/OrganizationConfig.h"
+#include "config/subconfigs/ServerConfig.h"
+#include "payment/datastructures/PaymentPayload.h"
+#include "payment/datastructures/PaymentRequiredResponse.h"
+#include "payment/datastructures/PaymentRequirements.h"
+#include "url/URLUtils.h"
+
+#include <folly/json.h>
+#include <boost/beast/core/detail/base64.hpp>
+
+
+FacilitatorProcessor::FacilitatorProcessor( MachinePayApp& app, ptr< IResponseSender >& responseSender )
+    : app_( app ), responseSender_( responseSender ) {
+    config_ = app_.configManager()->latestConfig();
+}
+
+
+
+
+void FacilitatorProcessor::reply502BadGateway( const std::string& message ) {
+    sendResponse( { 502, "Bad Gateway" }, STANDARD_HEADERS, message );
+
+    string body = getErrorBody( message );
+
+    sendResponse( { 502, "Bad Gateway" }, STANDARD_HEADERS, body );
+    state_ = State::ERROR_SENT;
+}
+
+void FacilitatorProcessor::reply200Success( const std::string& settlementInfo, std::string& proxiedBody ) {
+    std::vector< std::pair< std::string, std::string > > headers = {
+        { "Content-Type", "text/plain" }, { "X-PAYMENT-RESPONSE", settlementInfo }
+    };
+    sendResponse( { 200, "OK" }, headers, proxiedBody );
+    state_ = State::SUCCESS_RESOURCE_SENT;
+}
+
+
+
+
+
+
+std::string FacilitatorProcessor::getErrorBody( const std::string& message ) {
+    if ( organization_ && resource_ && config_ ) {
+        return PaymentRequiredResponse::getPaymentRequiredResponseAsString(
+            organization(), resource(), config(), message );
+    } else {
+        nlohmann::json j;
+        j["error"] = message;
+        return j.dump();
+    }
+}
+
+void FacilitatorProcessor::reply500InternalError( const std::string& message ) {
+    string body = getErrorBody( message );
+    sendResponse( { 500, "Server Error" }, STANDARD_HEADERS, body );
+    state_ = State::ERROR_SENT;
+}
+
+
+void FacilitatorProcessor::sendResponse( const std::pair< uint16_t, std::string >& statusAndMessage,
+    const std::vector< std::pair< std::string, std::string > >& headers, const std::string& body ) {
+    if ( state_ == State::ERROR_SENT ) {
+        spdlog::info( "Attempted to send response after error response already sent." );
+        return;
+    }
+
+    if ( state_ == State::SUCCESS_RESOURCE_SENT ) {
+        spdlog::info( "Attempted to send response after resource already sent." );
+        return;
+    }
+
+    if ( state_ == State::SUCCESS_PAYMENT_REQUIRED_SENT ) {
+        spdlog::info( "Attempted to send response after payment required already sent." );
+        return;
+    }
+
+    CHECK_STATE( responseSender_ );
+    try {
+        responseSender_->sendResponse( statusAndMessage, headers, body );
+    } catch ( std::exception& e ) {
+        spdlog::error( "Exception while sending response: {}", e.what() );
+        // nothing can be done so we consider response as sent
+    }
+}
+
+                          "(like localhost or xyz.com) to access this service." );
+        return false;
+    }
+
+    return true;
+}
+
+
+
+void FacilitatorProcessor::onRequestStart(
+    const std::unique_ptr< proxygen::HTTPMessage >& reqHeaders ) noexcept {
+    try {
+        CHECK_STATE( reqHeaders );
+
+    } catch ( std::exception& e ) {
+        spdlog::critical( "onRequestStart exception" );
+        printNestedException( e );
+        reply500InternalError( "Could not process x402 request start." );
+    };
+}
+
+
+void FacilitatorProcessor::replyToClientWithError( const HttpError& httpError ) {
+    auto httpErrorMessage = httpError.message();
+    switch ( httpError.type() ) {
+    case ErrorType::ERR_BAD_REQUEST:
+        reply400InvalidPayment( httpErrorMessage );
+        break;
+    case ErrorType::ERR_INTERNAL_SERVER_ERROR:
+        reply500InternalError( httpErrorMessage );
+        break;
+    case ErrorType::ERR_BAD_GATEWAY:
+        reply502BadGateway( httpErrorMessage );
+        break;
+    default:
+        // cant happen
+        CHECK_STATE( false );
+    }
+}
+
+void FacilitatorProcessor::onRequestFullyReceived(
+    const std::unique_ptr< proxygen::HTTPMessage >& reqHeaders, const string& body ) noexcept {
+    try {
+        if ( state_ == State::ERROR_SENT )
+            return;
+
+        CHECK_STATE( organization_ );
+        resource_ = organization_->getResourceByPath( decodedPath_, method_, body );
+
+
+        string responseBody;
+        if ( !proxyResponseToBackEnd( responseBody ) ) {
+            return;
+        }
+
+        auto settlementResponse = std::get< SettlementResponse >( result );
+
+        reply200Success( settlementResponse.originalJsonToBase64(), responseBody );
+    } catch ( std::exception& e ) {
+        spdlog::critical( "onRequestCompletion exception" );
+        printNestedException( e );
+        reply500InternalError( "Could not process x402 request." );
+    }
+}
+
+
+void FacilitatorProcessor::onBodySizeIncrease( size_t newSize ) {
+    constexpr size_t MAX_BODY_SIZE = 1024 * 1024;  // 128 KB
+    spdlog::info( "[onBodySizeIncrease] Request body size increased to {} bytes", newSize );
+    if ( newSize > MAX_BODY_SIZE ) {
+        reply400InvalidPayment(
+            "Request body too large. Maximum allowed is 1MByte. You can increase this limit in "
+            "machinepay config if needed." );
+    }
+}
+
+std::vector< std::pair< std::string, std::string > > FacilitatorProcessor::STANDARD_HEADERS = {
+    { "Content-Type", "application/json" }
+};
---
+++ src/x402_protocol/FacilitatorProcessor.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <proxygen/lib/http/HTTPMethod.h>
+#include <optional>
+
+#include "HttpError.h"
+#include "IResponseSender.h"
+#include "FacilitatorProcessorState.h"
+
+
+class SettlementResponse;
+class ResourceConfig;
+
+namespace proxygen {
+enum class HTTPMethod;
+class ResponseHandler;
+class HTTPMessage;
+}  // namespace proxygen
+
+class MachinePayApp;  // Forward declaration
+class MachinePayConfig;
+class OrganizationConfig;
+
+class FacilitatorProcessor {
+public:
+    using State = x402::State;
+    explicit FacilitatorProcessor( MachinePayApp& app, ptr< IResponseSender >& responseSender );
+
+    void onRequestStart( const std::unique_ptr< proxygen::HTTPMessage >& headers ) noexcept;
+    bool settle( std::string& responseBody );
+
+    void replyToClientWithError( const HttpError& httpError );
+
+    void onRequestFullyReceived(
+        const std::unique_ptr< proxygen::HTTPMessage >& reqHeaders, const string& body ) noexcept;
+    void onBodySizeIncrease( size_t newSize );
+    static std::vector< std::pair< std::string, std::string > > STANDARD_HEADERS;
+
+private:
+    void reply402PaymentRequired( std::optional< SettlementResponse > errorResponse );
+    void sendResponse( const std::pair< uint16_t, std::string >& statusAndMessage,
+        const std::vector< std::pair< std::string, std::string > >& headers,
+        const std::string& body );
+
+    void reply400InvalidPayment( const std::string& message );
+
+    string getErrorBody( const std::string& message );
+
+    void reply500InternalError( const std::string& message );
+
+    void reply502BadGateway( const std::string& message );
+
+
+    bool validateAndDecodePath( const std::unique_ptr< proxygen::HTTPMessage >& reqHeaders );
+
+
+    void reply200Success( const std::string& settlementInfo, std::string& proxyBody );
+
+    [[nodiscard]] ptr< MachinePayConfig > config() const {
+        CHECK_STATE( config_ );
+        return config_;
+    }
+
+
+    MachinePayApp& app_;
+    ptr< MachinePayConfig > config_;
+    std::string decodedPath_;
+    ptr< IResponseSender > responseSender_;
+    State state_ = State::START;
+    // initially set to non-supported value
+    proxygen::HTTPMethod method_ = proxygen::HTTPMethod::TRACE;
+};
