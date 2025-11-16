441bfe6148dcd9119d03149e83d71d71d631fb8a
kladkogex <13399135+kladkogex@users.noreply.github.com>
2025-11-16 23:21:04 +0000

    Refactor various files: improve type casting, enhance function signatures, and streamline error handling

--- run_machinepay.sh
+++ run_machinepay.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-exec /usr/bin/machinepay
+if [ ! -e /machinepay ]; then
+    echo "Error: /machinepay directory does not exist inside container " >&2
+    echo "You need to run machinepay container by mapping external volume into /machinepay"  >&2
+    exit 1
+fi
 
+exec chpst -C /machinepay /usr/bin/machinepay
