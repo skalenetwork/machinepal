1c3b0353478adba37be5efb8340694ef299d2165
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2025-12-23 18:58:29 +0000

    Improve local Docker image testing script by creating a temporary directory for initialization and adding success confirmation

--- docker/build_docker_image_locally.sh
+++ docker/build_docker_image_locally.sh
@@ -17,8 +17,8 @@ echo "Docker image $IMAGE_NAME built successfully."
 
 # test image
 
-mkdir machinepal
-cd machinepal && docker run -it -v $(pwd):/machinepal -e PUID=$(id -u) -e PGID=$(id -g) machinepal_image init
-ls -l machinepal
-rm -rf machinepal
+rm -rf ~/machinepal
+mkdir ~/machinepal
+cd ~/machinepal && docker run -it -v .:/machinepal -e PUID=$(id -u) -e PGID=$(id -g) machinepal_image init
+ls -l /home/kladko/machinepal_test1
 echo "Docker image $IMAGE_NAME tested successfully."
